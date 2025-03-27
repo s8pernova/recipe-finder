73f93c910f580bf1f020530ea0f68e331593dc59
achrunaway <achrunaway@gmail.com>
2025-03-27 10:56:55 -0400

    add in everything

---
+++ recipe-finder/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="main-container">
+			<h2>Hello World!</h2>
+		</div>
+	);
+};
+
+export default App;
---
+++ recipe-finder/src/components/ingredients.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ingredientsList from "../data/ingredients.json";
+
+const Ingredients = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default Ingredients;
